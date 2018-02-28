b038470f8c0f5e1f2d8426a1d438ecc663539778
pelehok <peleh.b97@gmail.com>
2018-02-28 06:40:59 +0200

    create Singleton pattern

---
+++ Singleton/Singleton/SingleObject.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class SingleObject
+    {
+        private static SingleObject instance = new SingleObject();
+
+        private SingleObject() { }
+
+        public static SingleObject getInstance()
+        {
+            return instance;
+        }
+        public void ShowMessage()
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
