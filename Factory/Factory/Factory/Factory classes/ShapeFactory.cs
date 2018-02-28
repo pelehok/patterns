56d132714572ac0dcdd500976e079eb519c1f8a0
pelehok <peleh.b97@gmail.com>
2018-02-28 06:34:15 +0200

    create factory pattern

---
+++ Factory/Factory/Factory/Factory classes/ShapeFactory.cs
@@ -0,0 +1,33 @@
+﻿using Factory.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory.Factory_classes
+{
+    class ShapeFactory
+    {
+        public IShape getShape(string shapeType)
+        {
+            if (shapeType == null)
+            {
+                return null;
+            }
+            if (shapeType.Equals("CIRCLE",StringComparison.CurrentCultureIgnoreCase))
+            {
+                return new Circle();
+            }
+            else if (shapeType.Equals("RECTANGLE", StringComparison.CurrentCultureIgnoreCase))
+            {
+                return new Rectangle();
+            }
+            else if (shapeType.Equals("SQUARE", StringComparison.CurrentCultureIgnoreCase))
+            {
+                return new Square();
+            }
+            return null;
+        }
+    }
+}
---
+++ Factory/Factory/Factory/FactoryPatternDemo.cs
@@ -0,0 +1,28 @@
+﻿using Factory.Factory_classes;
+using Factory.model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory
+{
+    class FactoryPatternDemo
+    {
+        static void Main(string[] args)
+        {
+            ShapeFactory shapeFactory = new ShapeFactory();
+
+            IShape shape1 = shapeFactory.getShape("Circle");
+            shape1.draw();
+
+            IShape shape2 = shapeFactory.getShape("Rectangle");
+            shape2.draw();
+
+            IShape shape3 = shapeFactory.getShape("Square");
+            shape3.draw();
+            Console.ReadKey();
+        }
+    }
+}
