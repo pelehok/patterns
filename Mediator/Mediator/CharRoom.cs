e0d268524eed10313ae20abe221f8841117ec4d1
Bogdan Peleh <peleh.b97@gmail.com>
2018-03-31 19:08:05 +0300

    added Mediator pattern

---
+++ Mediator/Mediator/CharRoom.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mediator
+{
+    class CharRoom
+    {
+        public static void ShowMessage(User user, string message){
+            Console.WriteLine(new DateTime() +
+                              " [ "+user.Name + " ] : "+message);
+        }
+    }
+}
---
+++ Mediator/Mediator/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mediator
+{
+    class User
+    {
+        public User(string name){
+            Name = name;
+        }
+
+        public string Name { get; set; }
+
+        public void SendMessage(string message){
+            CharRoom.ShowMessage(this,message);
+        }
+    }
+}
