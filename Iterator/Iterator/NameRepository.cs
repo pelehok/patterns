f5d234fedb31db42019c46c666938ba602d44a8e
Bogdan Peleh <peleh.b97@gmail.com>
2018-03-31 18:23:18 +0300

    added Iterator pattern

---
+++ Iterator/Iterator/NameRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Iterator
+{
+    class NameRepository:IContainer
+    {
+        public static string[] Names = {"Robert", "John", "Julie", "Lora"};
+
+        public IIterator GetIterator(){
+            return new NameIterator();
+        }
+        private class NameIterator:IIterator
+        {
+            private int index;
+
+            public bool HasNext(){
+                if (index < Names.Length)
+                {
+                    return true;
+                }
+
+                return false;
+            }
+
+            public object Next(){
+                if (this.HasNext())
+                {
+                    return Names[index++];
+                }
+
+                return null;
+            }
+        }
+    }
+}
---
+++ Iterator/Iterator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NameRepository namesRepository = new NameRepository();
+
+            for (IIterator iter = namesRepository.GetIterator(); iter.HasNext();)
+            {
+                string name = (string) iter.Next();
+                Console.WriteLine("Name: "+name);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
